30587b7fb4a949f0ed3d172875109729ae40b487
Maria Nikiforova <nikiforovamary@gmail.com>
2021-08-17 22:06:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './bootstrap.min.css';
+import Card from './components/Card.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Card
+      title="Card title"
+      text="Some quick example text to build on the card title and make up the bulk of the card's content.">
+        <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap">
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#868e96"></rect>
+          <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+        </svg>
+      </Card>
+
+      <Card
+        title="Special title treatment"
+        text="With supporting text below as a natural lead-in to additional content."
+      />
     </div>
   );
 }
